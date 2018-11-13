4257d2162f0475b6620ccea321679cdad0c62e3b
jackcao <jackcao@uw.edu>
2018-11-13 09:24:33 -0800

    update read-in

--- 01_data.py
+++ 01_data.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python3
 '''
 prepare data
 '''
+import os
 import pandas as pd
+import netCDF4 as ntc
+import xarray as xr
+from sys import platform
+
+if platform == "linux" or platform == "linux2":
+    sys_f = "/homes/jackcao/"
+else:
+    sys_f = "/Volumes/jackcao/"
+
+file_path = os.path.join(sys_f,"forecasting/sample/20181026_no_cap_to2100_spline/diabetes_male.nc")
+
+# read in netCDF data
+deaths_data = xr.open_dataset(file_path)
+
+# convert to pandas dataframe
+df = deaths_data.to_dataframe()
